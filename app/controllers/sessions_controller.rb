a436297c3b397c90e8eaf8fceade0f70825ed04e
Danish Satkut <danish.satkut@infibeam.net>
2015-07-15 12:13:31 +0530

    Implement logout mechanism.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,13 @@ class SessionsController < ApplicationController
   end
 
   def home
-    
+    current_user = User.where(id: session[:user_id]).first
+
+    redirect_to login_path unless current_user
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
   end
 end
